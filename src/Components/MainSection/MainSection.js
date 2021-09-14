cf3384c8b3d5cec21b95db0b46b9f8bf0abc6ad9
Yatin Burhmi <yatinburhmi@Yatins-MacBook-Air.local>
2021-09-14 11:38:10 -0400

    HeroSection Completed

---
+++ src/Components/MainSection/MainSection.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Typist from 'react-typist'
+import Video from '../../assets/video-bg/video.mp4'
+import { HeroContainer, HeroBackground, VideoBg, HeroContent, HeroH1, HeroName, HeroP} from './MainSectionElements'
+
+export const MainSection = () => {
+    return (
+        <>
+            <HeroContainer>
+                <HeroBackground>
+                    <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
+                </HeroBackground>
+                <HeroContent>
+                    <HeroH1>
+                        Hello, I'm <HeroName>Yatin Burhmi.</HeroName>
+                    </HeroH1>
+                    <HeroP>
+                        <Typist>
+                        <Typist.Delay ms={1500} />
+                            I am a full-stack web developer.
+                        </Typist>
+                    </HeroP>
+                    
+                </HeroContent>
+            </HeroContainer>
+        </>
+    )
+}
