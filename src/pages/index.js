363ea23a27e7a0611e4d7052a0f210f47924de18
Yatin Burhmi <yatinburhmi@Yatins-MacBook-Air.local>
2022-05-27 18:13:44 -0400

    Netlify build err resolved

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar';
-import { MainSection } from "../components/MainSection"
-import { AboutSection } from '../components/AboutSection';
-import { ProjectSection } from '../components/ProjectSection';
-import { ContactSection } from '../components/ContactSection';
-import { Footer } from '../components/Footer';
+import Sidebar from '../Components/Sidebar';
+import Navbar from '../Components/Navbar';
+import { MainSection } from "../Components/MainSection"
+import { AboutSection } from '../Components/AboutSection';
+import { ProjectSection } from '../Components/ProjectSection';
+import { ContactSection } from '../Components/ContactSection';
+import { Footer } from '../Components/Footer';
 
 
 
